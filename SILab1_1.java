a53872c6be39d402437a86e344ab4c6a34c6a9ec
kranoxje <draganbuckovski@gmail.com>
2024-04-16 19:47:37 +0200

    Fix wrong condition in filterOddLengthStrings.

--- SILab1_1.java
+++ SILab1_1.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SILab1_1 {
-    public static List<String> filterOddLengthStrings(List<String> list) {
+    public static List<String> filterOddLengthStrings(List<String> strings) {
         List<String> result = new ArrayList<>();
-        for (String el : list) {
-            if (el.length() % 2 == 0) {
-                result.add(el);
+        for (String s : strings) {
+            if (s.length() % 2 == 0) {
+                result.add(s);
             }
         }
         return result;
